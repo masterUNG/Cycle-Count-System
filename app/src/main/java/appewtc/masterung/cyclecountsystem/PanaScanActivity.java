0d93141e1ea79f7c8090d4ee611d392f4a6da3d2
masterUNG <phrombutr@gmail.com>
2016-10-07 16:39:49 +0700

    Near Success

--- app/src/main/java/appewtc/masterung/cyclecountsystem/PanaScanActivity.java
+++ app/src/main/java/appewtc/masterung/cyclecountsystem/PanaScanActivity.java
@@ -1,13 +1,15 @@
 package appewtc.masterung.cyclecountsystem;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -40,7 +42,20 @@ public class PanaScanActivity extends AppCompatActivity {
 
                 Log.d("7octV4", "strReadCode ==> " + strReadCode);
 
-                createListView(strReadCode);
+                editText.setText("");   // Clear Edit Text
+
+                if(editText.requestFocus()) {
+                    getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+                }
+
+                if (strReadCode.equals("")) {
+                    //Have Space
+                    Toast.makeText(PanaScanActivity.this, "Scan ก่อนซิ", Toast.LENGTH_SHORT).show();
+                } else {
+                    createListView(strReadCode);
+                }
+
+
 
             }   // onClick
         });
